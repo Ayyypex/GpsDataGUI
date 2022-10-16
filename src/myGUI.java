a0025a8afbf51e2bd3db4f1edf9264767b4ca688
Spencer <a1765159@adelaide.edu.au>
2022-10-16 19:50:37 +1030

    minor changes for semantic reasons

--- src/myGUI.java
+++ src/myGUI.java
@@ -3,59 +3,58 @@ import nz.sodium.*;
 import swidgets.*;
 
 /** My GUI which displays transformed tracker data retrieved from a stream using Sodium FRP operations. */
-public class myGUI {
+public class myGUI extends JFrame {
 
   // need these while creating the components
-  public JFrame gui;                   // maybe not
+  public JFrame gui;                           // maybe not
   public Stream<GpsEvent>[] streams;
 
-  /** Creates an instance of myGUI */
+  // arrays for simplifiedTrackersComponent (stc)
+  public Cell<GpsEvent>[] stcCELLS;
+  public SLabel[] stcLABELS;
+
+  /** Creates an instance of myGUI and then shows it. */
   public static void main(String[] args) {
-    new myGUI();
+    myGUI gui = new myGUI();
+    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    gui.setLocationRelativeTo(null);  // place gui window at center of screen
+    gui.setVisible(true);
   }
 
-  /** Constructs and then shows my GUI. */
+  /** Constructs my GUI. */
   public myGUI() {
-    
-    // create and configure gui
-    gui = new JFrame();
-    gui.setTitle("a1765159's GUI");
-    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    gui.setResizable(false);
-    gui.setSize(1600, 900);
+    this.setTitle("a1765159's GUI");
+    this.setResizable(false);
+    this.setSize(1600, 900);
 
     // get all event streams
     GpsService serv = new GpsService();
     streams = serv.getEventStreams();
     
     // add components to gui
-    gui.add( new simplifiedTrackersComponent() );
-    gui.add( new allEventsComponent() );
-    gui.add( new eventsWithinRangeComponent() );
-    gui.add( new distanceTravelledComponent() );
-
-    // show gui
-    gui.setLocationRelativeTo(null);  // place gui window at center of screen
-    gui.setVisible(true);
+    this.add( new simplifiedTrackersComponent() );
+    this.add( new allEventsComponent() );
+    this.add( new eventsWithinRangeComponent() );
+    this.add( new distanceTravelledComponent() );
   }
 
   /** Displays ten simplified tracker displays, stripping the altitude from a GpsEvent. */
-  private class simplifiedTrackersComponent extends JPanel {
+  public class simplifiedTrackersComponent extends JPanel {
     //
   }
 
   /** Displays all GpsEvents passed to the GUI at the time they occur. */
-  private class allEventsComponent extends JPanel {
+  public class allEventsComponent extends JPanel {
     //
   }
 
   /** Displays all GpsEvents within a latitude and longitude range set by a user-configurable control panel. */
-  private class eventsWithinRangeComponent extends JPanel {
+  public class eventsWithinRangeComponent extends JPanel {
     //
   }
 
   /** Displays the distance travelled over the last 5 minutes for each tracker within the latitude/longitude range. */
-  private class distanceTravelledComponent extends JPanel {
+  public class distanceTravelledComponent extends JPanel {
     //
   }
 }

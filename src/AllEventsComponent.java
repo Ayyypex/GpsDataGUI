e230367e6e56eb63320c043102936dba2fe47acb
Spencer <a1765159@adelaide.edu.au>
2022-10-20 23:41:09 +1030

    finished implementing the AllEventsComponent

--- src/AllEventsComponent.java
+++ src/AllEventsComponent.java
@@ -11,17 +11,22 @@ public class AllEventsComponent extends JPanel {
   boolean Testing = true;
   public Cell<String> cell;
 
-  ///////////
+  /**
+   * Returns a stream that fires an event at specified intervals.
+   * 
+   * @param sys     A Sodium FRP timer system.
+   * @param period  The specified interval that the stream will fire.
+   * @return        A timer in the form of a stream that will repeatedly fire periodically.
+   */
   static Stream<Long> periodic(TimerSystem sys, long period) {
     Cell<Long> time = sys.time;
     CellLoop<Optional<Long>> oAlarm = new CellLoop<>();
     Stream<Long> sAlarm = sys.at(oAlarm);
     oAlarm.loop(
-    sAlarm.map(t -> Optional.of(t + period))
-    .hold(Optional.<Long>of(time.sample() + period)));
+      sAlarm.map(t -> Optional.of(t + period))
+        .hold(Optional.<Long>of(time.sample() + period)));
     return sAlarm;
   }
-  ////////////
 
   /**
    * Merges an array of GpsEvent streams 
@@ -39,49 +44,48 @@ public class AllEventsComponent extends JPanel {
    * @param  streams   An array of GpsEvent streams that will be displayed.
    */
   public AllEventsComponent(Stream<GpsEvent>[] streams) {
-    ///////////
-    TimerSystem sys = new MillisecondsTimerSystem();
-    Cell<Long> time = sys.time;
-    ///////////
-
     // configure main panel
     this.setLayout(new GridBagLayout());
 
+    // set up Sodium FRP timer system and cell to hold the current time
+    TimerSystem sys = new MillisecondsTimerSystem();
+    Cell<Long> time = sys.time;
+
     // merge streams
-    Stream<GpsEvent> allStreams = streams[0];//mergeStreams(streams);
+    Stream<GpsEvent> allStreams = mergeStreams(streams);
 
-    // set up cells to hold the event as a string and the time at which it occurs
-    Cell<String> eventString = allStreams.map( (GpsEvent ev) -> ev.toString() ).hold("");
-    Cell<String> timeStamp = allStreams.map( (GpsEvent ev) -> "at: " + String.valueOf(java.time.LocalTime.now()) ).hold("");
-
-    /////////
-    Stream<Long> timeStamp1 = allStreams.map( (GpsEvent ev) -> time.sample() );
-    Cell<Long> lastEventTime = timeStamp1.hold(Long.valueOf(0));
-      /////////
-
-    // create SLabels
-    SLabel eventStringLabel = new SLabel(eventString);
-    SLabel timeStampLabel = new SLabel(timeStamp);
-
-    ///////////////////
-    Transaction.runVoid(() -> {
-      Stream<String> clearStream = periodic(sys, 100).map((t)->String.valueOf(time.sample()));//.snapshot(lastEventTime).filter( a -> (a - lastEventTime.sample()) < 3000 ).map(t->"");
-      Stream<String> clearStream2 = clearStream.snapshot(lastEventTime).filter(t->((time.sample() - lastEventTime.sample()) > 3000)).map(t->"");
-      Cell<String> eventString1 = allStreams.map( (GpsEvent ev) -> ev.toString() ).orElse(clearStream2).hold("");
-      SLabel eventStringLabel1 = new SLabel(eventString1);
-      this.add(eventStringLabel1);
-    });
-    /////////////////
+    // record system time of last event occurrence
+    Cell<Long> lastEventTime = allStreams.map( (GpsEvent ev) -> time.sample() ).hold(Long.valueOf(0));
 
     // create and configure event panel that info will be displayed in
     JPanel eventPanel = new JPanel();
     eventPanel.setBackground(Color.white);
     eventPanel.setBorder(BorderFactory.createEtchedBorder());
     eventPanel.setPreferredSize( new Dimension(500, 50) );
+    
+    // loop() in peridioc can only run in a Transaction 
+    Transaction.runVoid(() -> {
+      // create stream that will fire an event every 0.1 seconds
+      Stream<Long> timerStream = periodic(sys, 100);
 
-    // add labels to display
-    eventPanel.add(eventStringLabel);
-    eventPanel.add(timeStampLabel);
+      // create CellLoop for eventString, as the clearStream will check whether it is empty or not, so that it doesn't repeatedly fire an empty string
+      CellLoop<String> eventString = new CellLoop<>();
+
+      // at every event, produce empty string if the time between the last event and now has exceeded 3 seconds and the eventString isn't already empty
+      Stream<String> clearStream = timerStream.filter( (Long t) -> ((time.sample() - lastEventTime.sample()) > 3000) && (eventString.sample() != "")  ).map((Long t) -> "");
+
+      // set up cells to hold the event and the time at which it occurs as separate strings
+      eventString.loop( allStreams.map( (GpsEvent ev) -> ev.toString() ).orElse(clearStream).hold("") );
+      Cell<String> timeStampString = allStreams.map( (GpsEvent ev) -> "at: " + String.valueOf(java.time.LocalTime.now()) ).orElse(clearStream).hold("");
+
+      // create SLabels
+      SLabel eventStringLabel = new SLabel(eventString);
+      SLabel timeStampLabel = new SLabel(timeStampString);
+
+      // add labels to display
+      eventPanel.add(eventStringLabel);
+      eventPanel.add(timeStampLabel);
+    });
 
     // add to main panel
     this.add(eventPanel);

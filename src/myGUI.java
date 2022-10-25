3e33eb5644c71631c33ec0a0197faa75341e55aa
Spencer <a1765159@adelaide.edu.au>
2022-10-26 00:08:34 +1030

    began refactoring/restructuring of code - first display complete

--- src/myGUI.java
+++ src/myGUI.java
@@ -1,17 +1,18 @@
 import javax.swing.*;
-import javax.swing.border.*;
 import java.awt.*;
 import nz.sodium.*;
 import nz.sodium.time.*;
+import swidgets.*;
 import java.util.*;
+
+// supress warnings from converting the linked list of streams to an array
+@SuppressWarnings("unchecked")
 
 /** 
  * My GUI which displays transformed tracker data retrieved from a stream
  * using Sodium FRP operations. 
  */
 public class myGUI extends JFrame {
-
-  boolean Testing = true;
 
   /** Creates an instance of myGUI and then shows it. */
   public static void main(String[] args) {
@@ -25,25 +26,152 @@ public class myGUI extends JFrame {
   public myGUI() {
     // mandatory configuration
     this.setTitle("a1765159's GUI");
-    Border blackline = BorderFactory.createLineBorder(Color.black, 5);
-    this.getRootPane().setBorder(blackline);
     this.setBackground(Color.gray);
-    this.setSize(1600, 900);
+    this.setSize(1920, 1080);
     
     // get the tracker's event streams
     GpsService serv = new GpsService();
     Stream<GpsEvent>[] streams = serv.getEventStreams();
 
-    // create control panel for the last two displays
-    ControlPanelComponent ctrlPnl = new ControlPanelComponent(null);
+    // construct table header
+    JPanel trackersTablePanel = new JPanel(new GridBagLayout());
+
+    // create header labels
+    JLabel trackerNumHeader = new JLabel("Tracker Number");
+    JLabel trackerLatHeader = new JLabel("Latitude");
+    JLabel trackerLonHeader = new JLabel("Longitude");
+    JLabel trackerDistDeader = new JLabel("Distance Travelled (meters)");
+
     
-    // add components to gui
-    JTabbedPane tabbedPane = new JTabbedPane();
-    this.add(tabbedPane);
-    tabbedPane.addTab( "Simplified Trackers", new SimplifiedTrackersComponent(streams) );
-    tabbedPane.addTab( "All Events", new AllEventsComponent(streams) );
-    tabbedPane.addTab( "Events within Range", new EventsWithinRangeComponent(streams, ctrlPnl, null) );
-    tabbedPane.addTab( "Distance Travelled within Range", new DistanceTravelledComponent(streams, ctrlPnl) );
+    // configure header labels
+    trackerNumHeader.setHorizontalAlignment(SwingConstants.CENTER);
+    trackerLatHeader.setHorizontalAlignment(SwingConstants.CENTER);
+    trackerLonHeader.setHorizontalAlignment(SwingConstants.CENTER);
+    trackerDistDeader.setHorizontalAlignment(SwingConstants.CENTER);
+
+    // define insets to be used throughout table
+    Insets tableRowInsets = new Insets(10, 50, 10, 50);
+    Insets seperatorInsets = new Insets(2, 0, 2, 0);
+
+    // add table headers
+    addComponent(trackersTablePanel, trackerNumHeader, 0, 0, 1, 1, tableRowInsets);
+    addComponent(trackersTablePanel, trackerLatHeader, 0, 1, 1, 1, tableRowInsets);
+    addComponent(trackersTablePanel, trackerLonHeader, 0, 2, 1, 1, tableRowInsets);
+    addComponent(trackersTablePanel, trackerDistDeader, 0, 4, 1, 1, tableRowInsets);
+
+    // add table header separator
+    JSeparator headerSep = new JSeparator(SwingConstants.HORIZONTAL);
+    headerSep.setForeground(Color.black);
+    headerSep.setBackground(Color.black);
+    addComponent(trackersTablePanel, headerSep, 1, 0, 5, 1, seperatorInsets);
+
+    // add vertical separator to indicate difference between the distance column
+    JSeparator distanceSep = new JSeparator(SwingConstants.VERTICAL);
+    distanceSep.setForeground(Color.black);
+    distanceSep.setBackground(Color.black);
+    GridBagConstraints c = new GridBagConstraints();
+    c.fill = GridBagConstraints.VERTICAL;
+    c.gridx=3;  
+    c.gridy=0;
+    c.gridwidth=1;
+    c.gridheight=22;
+    trackersTablePanel.add(distanceSep, c);
+
+    int rowIndex = 2;   // row to put the tracker info in, because separator takes a row
+
+    // fill row for each stream/tracker    
+    for ( int i=0; i<streams.length; i++ ) {
+      // get stream of GpsEvents for the tracker
+      Stream<GpsEvent> sGpsEvents = streams[i];
+
+      // get simplified tracker info
+      Cell<String>[] simplifiedGpsCells =  getSimplifiedGpsCells(sGpsEvents);
+
+      // create SLabels
+      SLabel trackerNumLabel = new SLabel(simplifiedGpsCells[0]);
+      SLabel trackerLatLabel = new SLabel(simplifiedGpsCells[1]);
+      SLabel trackerLonLabel = new SLabel(simplifiedGpsCells[2]);
+      
+      // configure SLabels
+      trackerNumLabel.setHorizontalAlignment(SwingConstants.CENTER);
+      trackerLatLabel.setHorizontalAlignment(SwingConstants.CENTER);
+      trackerLonLabel.setHorizontalAlignment(SwingConstants.CENTER);
+      Font tableValueFont = new Font("Courier", Font.PLAIN, 14);
+      trackerNumLabel.setFont(tableValueFont);
+      trackerLatLabel.setFont(tableValueFont);
+      trackerLonLabel.setFont(tableValueFont);
+
+      // add SLabels to table
+      addComponent( trackersTablePanel, trackerNumLabel, rowIndex, 0, 1, 1, tableRowInsets );
+      addComponent( trackersTablePanel, trackerLatLabel, rowIndex, 1, 1, 1, tableRowInsets );
+      addComponent( trackersTablePanel, trackerLonLabel, rowIndex, 2, 1, 1, tableRowInsets );
+      addComponent( trackersTablePanel, new JLabel("temporary4"), rowIndex, 4, 1, 1, tableRowInsets );
+
+      // add separator for each row
+      JSeparator rowSep = new JSeparator(SwingConstants.HORIZONTAL);
+      addComponent( trackersTablePanel, rowSep, rowIndex-1, 0, 5, 1, seperatorInsets );
+      rowIndex+=2;
+    }
+
+    // add to frame
+    this.add(trackersTablePanel);
+  }
+
+  /**
+   * Adds a component to a JPanel with the specified GridBag contraints.
+   * @param panel       The JPanel the component is being added to.
+   * @param component   The component to be added.
+   * @param row         The row to be added to.
+   * @param col         The column to be added to.
+   * @param width       The number of columns the component will occupy. 
+   * @param height      The number of rows the component will occupy. 
+   */
+  private void addComponent(JPanel panel, Component component,
+    int row, int col, int width, int height, Insets insets) 
+  {
+    GridBagConstraints c = new GridBagConstraints();
+    c.insets = insets;
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx=col;  
+    c.gridy=row;
+    c.gridwidth=width;
+    c.gridheight=height;
+    panel.add(component, c);
+  }
+
+  /**
+   * Strips a GpsEvent stream of its altitude. 
+   * 
+   * @param   stream    A stream of GpsEvents
+   * @return  A stream of SimpleGpsEvents
+   */
+  public static Stream<SimpleGpsEvent> stripAltitude(Stream<GpsEvent> stream) {
+    return stream.map( (GpsEvent ev) -> new SimpleGpsEvent(ev) );
+  }
+
+  /**
+   * 
+   * @param sGpsEvents
+   * @return
+   */
+  public static Cell<String>[] getSimplifiedGpsCells( Stream<GpsEvent> sGpsEvents) {
+    // set up cell to hold the stripped stream's SimpleGpsEvent
+    Cell<SimpleGpsEvent> cEvent = stripAltitude(sGpsEvents)
+      .hold( new SimpleGpsEvent() );
+
+    // set up cells to hold each field
+    Cell<String> cTrackerNumber = cEvent.map( (SimpleGpsEvent ev) -> ev.getTrackerNumber() );
+    Cell<String> cTrackerLatitude = cEvent.map( (SimpleGpsEvent ev) -> ev.getLatitude() );
+    Cell<String> cTrackerLongitude = cEvent.map( (SimpleGpsEvent ev) -> ev.getLongitude() );
+
+    // create linked list, so we can convert it to array
+    LinkedList<Cell<String>> cells = new LinkedList<Cell<String>>();
+    cells.add(cTrackerNumber);
+    cells.add(cTrackerLatitude);
+    cells.add(cTrackerLongitude);
+
+    // return the linked list in array form
+    return (Cell<String>[])cells.toArray(new Cell[0]);
   }
 
   /**
